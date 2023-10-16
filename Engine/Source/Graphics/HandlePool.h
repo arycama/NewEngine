2cf35b8948a4fab4f1fdd6c0b8180b91153d8ce5
Ben Sims <arycama@gmail.com>
2023-10-16 18:11:58 +1030

    Handlepool wip

--- Engine/Source/Graphics/HandlePool.h
+++ Engine/Source/Graphics/HandlePool.h
@@ -1,3 +1,7 @@
+#pragma once
+
+#include "Handle.h"
+
 #include <queue>
 #include <vector>
 
@@ -5,12 +9,50 @@ template <typename T>
 class HandlePool
 {
 public:
-	HandlePool();
-	~HandlePool();
-	Handle CreateHandle(const T& resource);
-	void ReleaseHandle(const Handle& handle);
+	~HandlePool()
+	{
+		for (auto& resource : resources)
+			if (resource.first)
+				resource.first->Release();
+	}
 
-	T& GetResource(const Handle& handle) const;
+	Handle CreateHandle(const T& resource)
+	{
+		// Find an index
+		if (availableIndices.empty())
+		{
+			// No more indices, add new one and return it's value
+			auto index = resources.size();
+			resources.push_back(std::make_pair(resource, 0));
+			return Handle(index, 0);
+		}
+		else
+		{
+			// Get the next available index and insert
+			auto& index = availableIndices.front();
+			availableIndices.pop();
+
+			// Increment the version
+			index.second++;
+			resources[index.first] = std::make_pair(resource, index.second);
+
+			return Handle(index.first, index.second);
+		}
+	}
+
+	void ReleaseHandle(const Handle& handle)
+	{
+		auto& data = resources.at(handle.GetIndex());
+		data.first->Release();
+		data.first = nullptr;
+		availableIndices.push(std::make_pair(handle.GetIndex(), handle.GetVersion()));
+	}
+
+	T& GetResource(const Handle& handle) const
+	{
+		return *resources.at(handle.GetIndex()).first;
+	}
+
 
 private:
 	std::vector<std::pair<T*, int>> resources;
